1d9f193f733a791021a798303202f176d76c4f4c
(cdep)illabout <cdep.illabout@gmail.com>
2015-05-06 13:29:03 +0900

    Fixed up and added documentation to some of the test files.

--- test/EmbedTree.hs
+++ test/EmbedTree.hs
@@ -1,9 +1,14 @@
 {-# LANGUAGE TemplateHaskell, TupleSections #-}
-module EmbedTree where
+module EmbedTree ( Entry(..)
+                 , getFile
+                 , getDirectory
+                 , getEntry
+                 , embedTree
+                 ) where
 
-import Control.Applicative ((<$>))
 import qualified Data.Map as Map
 import Data.Map (Map)
+import Data.Maybe (fromMaybe)
 
 import Instances.TH.Lift ()
 
@@ -18,26 +23,40 @@ import System.Directory
   )
 import System.FilePath (takeDirectory, (</>))
 
-data Entry = File String | Directory (Map String Entry)
+-- | This represents a directory listing.  It can either
+-- a file with it's contents, or a directory, containing
+-- a map of filenames to entries.
+data Entry = File String -- ^ The contents of a file.
+           | Directory (Map String Entry) -- ^ Mapping of filenames to 'Entry's.
+           deriving Show
 
+-- | Get the 'File' contents out of an entry.  Returns contents of a file
+-- if this 'Entry' is actually a 'File', otherwise throws an error.
 getFile :: Entry -> String
 getFile (File contents) = contents
-getFile (Directory _) = error $ "getFile: found a directory"
+getFile (Directory _) = error "getFile: found a directory"
 
+-- | Get the directory map contents out of an entry.  Returns directory map
+-- if this 'Entry' is actually a 'Directory', otherwise throws an error.
 getDirectory :: Entry -> Map String Entry
-getDirectory (File _) = error $ "getDirectory: found a file"
+getDirectory (File _) = error "getDirectory: found a file"
 getDirectory (Directory contents) = contents
 
-getEntry :: String -> Map String Entry -> Entry
+-- | Returns the 'Entry' corresponding to a filename in a Directory 'Map'.
+-- Throws an error if the filename is not found.
+getEntry :: String              -- ^ The filename to search for.
+         -> Map String Entry    -- ^ Filename to 'Entry' 'Map' to search in.
+         -> Entry               -- ^ Corresponding 'Entry'
 getEntry name entries =
-  case Map.lookup name entries of
-    Nothing -> error $ "getEntry: " ++ name ++ " not found"
-    Just v -> v
+    fromMaybe (error $ "getEntry: " ++ name ++ " not found")
+              (Map.lookup name entries)
 
 instance Lift Entry where
   lift (File contents) = AppE (ConE 'File) <$> [|contents|]
   lift (Directory entries) = AppE (ConE 'Directory) <$> [|entries|]
 
+-- | Create an 'Entry' that represents a directory tree with the
+-- root being the first argument to this function.
 readTree :: FilePath -> IO Entry
 readTree path = do
   isFile <- doesFileExist path
@@ -51,6 +70,16 @@ readTree path = do
           Directory . Map.fromList <$> mapM (\entry -> (entry,) <$> readTree (path </> entry)) entries
         else fail $ path ++ " is not a file or directory"
 
+-- | Create and embed an 'Entry' using 'readTree'.  This should be used
+-- from Template Haskell like the following:
+--
+-- @
+--  files :: Entry
+--  files = $(embedTree "my-directory")
+-- @
+--
+-- @my-directory@ will be looked for as a relative path from the current
+-- source directory.
 embedTree :: FilePath -> ExpQ
 embedTree relPath = do
     t <- runIO (readTree (takeDirectory $__FILE__ </> relPath))
