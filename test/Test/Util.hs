c648b67ac3e9d99710c83664568578d5fe889879
(cdep)illabout <cdep.illabout@gmail.com>
2015-05-11 16:06:49 +0900

    Made test utils compile on GHC7.6.

--- test/Test/Util.hs
+++ test/Test/Util.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE CPP #-}
+
 module Test.Util where
+
+#if __GLASGOW_HASKELL__ < 710
+import Control.Applicative
+#endif
 
 import qualified Data.ByteString.Lazy as LBS
 import Test.QuickCheck (Arbitrary(..))
