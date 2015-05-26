a5ee5b69b7dd971524d4f054696639f028b24409
(cdep)illabout <cdep.illabout@gmail.com>
2015-05-27 00:29:50 +0900

    Added two tests for packetLength.

--- test/Test/SSH/Packet.hs
+++ test/Test/SSH/Packet.hs
@@ -7,18 +7,27 @@ import Control.Applicative
 #endif
 
 import qualified Data.ByteString.Char8 as Char8
-import Test.HUnit (assertBool)
+import Test.HUnit ((@?=), assertBool)
 import Test.QuickCheck (choose, elements, listOf)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase)
 import Test.Tasty.QuickCheck (testProperty)
 
-import SSH.Packet ()
+import SSH.Packet (byte, long, packetLength)
 
 import Test.Util (ArbitraryLazyByteString(..))
 
+packetLengthTestEmptyPacket :: TestTree
+packetLengthTestEmptyPacket = testCase "test for packetLength with empty Packet" $
+    packetLength (return ()) @?= 0
+
+packetLengthTest :: TestTree
+packetLengthTest = testCase "simple test for packetLength" $
+    packetLength (byte 1 >> long 1) @?= 5
+
 sshPacketTests :: TestTree
 sshPacketTests = testGroup "SSH/Packet.hs tests"
-    [
+    [ packetLengthTest
+    , packetLengthTestEmptyPacket
     ]
 
