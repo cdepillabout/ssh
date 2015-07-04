797aa2a8bd25a32a0f4c1d2166d7a14e993d9198
(cdep)illabout <cdep.illabout@gmail.com>
2015-07-05 00:52:17 +0900

    Remove uneeded imports from Sender.hs tests.

--- test/Test/SSH/Sender.hs
+++ test/Test/SSH/Sender.hs
@@ -7,22 +7,13 @@ module Test.SSH.Sender (sshSenderTests) where
 import Control.Applicative
 #endif
 
-import qualified Data.ByteString.Lazy as LBS
-import Data.Word (Word8)
-import Test.QuickCheck.Monadic (monadicIO, pick)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.QuickCheck (arbitrary, choose, testProperty, vectorOf)
-
-import SSH.Crypto
-
-import Test.Util (ArbitraryLBS(..), assertM, publicKey)
 
 -----------
 -- Tests --
 -----------
 
-blahblahTest :: TestTree
-blahblahTest = undefined
+-- | TODO: tests.
 
 sshSenderTests :: TestTree
 sshSenderTests = testGroup "SSH/Sender.hs tests"
