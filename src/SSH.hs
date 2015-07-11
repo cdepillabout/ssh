46538a3a066d8c21a1cca1f6573da0855fcd1021
(cdep)illabout <cdep.illabout@gmail.com>
2015-07-11 14:30:41 +0900

    Adding some notes and type signatures.

--- src/SSH.hs
+++ src/SSH.hs
@@ -15,6 +15,7 @@ import Network
 import OpenSSL.BN (randIntegerOneToNMinusOne, modexp)
 import System.IO
 import System.Random
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Map as M
 import qualified Data.Serialize as S
@@ -32,34 +33,44 @@ import SSH.Internal.Util
 version :: String
 version = "SSH-2.0-DarcsDen"
 
+-- | The required key exchange algorithms are specified in
+-- <https://tools.ietf.org/html/rfc4253#section-6.5 rfc4253 section 6.5>.
 supportedKeyExchanges :: [String]
 supportedKeyExchanges =
     [ "diffie-hellman-group1-sha1"
+    -- TODO: The SSH rfc requires that the following method is also
+    -- supported.
     -- , "diffie-hellman-group-exchange-sha1"
     ]
 
+-- | The required
 supportedKeyAlgorithms :: [String]
 supportedKeyAlgorithms = ["ssh-rsa", "ssh-dss"]
 
+-- | 
 supportedCiphers :: [(String, Cipher)]
 supportedCiphers =
-    [ ("aes256-cbc", aesCipher CBC 32)
-    , ("aes192-cbc", aesCipher CBC 24)
-    , ("aes128-cbc", aesCipher CBC 16)
+    [ ("aes256-cbc", aesCipher 32)
+    , ("aes192-cbc", aesCipher 24)
+    , ("aes128-cbc", aesCipher 16)
     ]
   where
-    aesCipher m s =
-        Cipher AES m 16 s
+    aesCipher :: Int -> Cipher
+    aesCipher s = Cipher AES CBC 16 s
 
+-- | The required macs are specified in
+-- <https://tools.ietf.org/html/rfc4253#section-6.4 rfc4253 section 6.4>.
 supportedMACs :: [(String, LBS.ByteString -> HMAC)]
 supportedMACs =
-    [ ("hmac-sha1", makeHMAC True)
-    , ("hmac-md5", makeHMAC False)
+    [ ("hmac-sha1", sha)
+    , ("hmac-md5", md5)
     ]
   where
-    makeHMAC True k = HMAC 20 $ \b -> bsToLBS . S.runPut $ S.put (hmac (MacKey (strictLBS (LBS.take 20 k))) b :: SHA1)
-    makeHMAC False k = HMAC 16 $ \b -> bsToLBS . S.runPut $ S.put (hmac (MacKey (strictLBS (LBS.take 16 k))) b :: MD5)
+    sha, md5 :: LBS.ByteString -> HMAC
+    sha k = HMAC 20 $ \b -> bsToLBS . S.runPut $ S.put (hmac (MacKey (strictLBS (LBS.take 20 k))) b :: SHA1)
+    md5 k = HMAC 16 $ \b -> bsToLBS . S.runPut $ S.put (hmac (MacKey (strictLBS (LBS.take 16 k))) b :: MD5)
 
+    bsToLBS :: BS.ByteString -> LBS.ByteString
     bsToLBS = LBS.fromChunks . (: [])
 
 supportedCompression :: String
