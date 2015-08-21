9216c0f3f284d2a7da7cef3d5dc8140f2764f67e
(cdep)illabout <cdep.illabout@gmail.com>
2015-08-21 16:27:44 +0900

    Finish cleaning up.

--- src/SSH/Supported.hs
+++ src/SSH/Supported.hs
@@ -1,31 +1,10 @@
 module SSH.Supported where
 
-import Control.Concurrent (forkIO)
-import Control.Concurrent.Chan (newChan, writeChan)
-import Control.Exception.Lifted (bracket)
-import Control.Lens ((^.), (.~), Lens', lens, makeClassy, set, view)
-import Control.Monad (replicateM)
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Reader (ReaderT, runReaderT)
-import Control.Monad.Reader.Class (MonadReader, asks)
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.Control (MonadBaseControl)
-import Control.Monad.Trans.State (evalStateT, get, gets, modify)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
-import Data.Digest.Pure.SHA (bytestringDigest, sha1)
-import qualified Data.Map as M
 import qualified Data.Serialize as S
 import Crypto.HMAC (MacKey(..), hmac)
 import Crypto.Hash.CryptoAPI (MD5, SHA1)
-import Data.List (intercalate)
-import Data.List.Split (splitOn)
-import Network (
-    PortID(PortNumber), PortNumber, Socket, accept,listenOn, sClose,
-    )
-import OpenSSL.BN (randIntegerOneToNMinusOne, modexp)
-import System.IO (hFlush, hGetLine, hIsEOF, hPutStr, hSetBinaryMode)
-import System.Random (randomRIO)
 
 import SSH.Crypto
 import SSH.Internal.Util
